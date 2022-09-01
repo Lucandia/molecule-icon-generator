76c94d08666f5f397e019aba261080e73617ee99
lmonari5 <monarluca@gmail.com>
2022-09-01 05:19:47 +0200

    improved help

--- streamlit-app.py
+++ streamlit-app.py
@@ -12,30 +12,29 @@ import os
 import cv2
 import molecules_icon_generator as mig
 
-
 if __name__ == "__main__":
-    
+
     # select the folder with the atom icons:
     atom_icon_dir = "base-icons/"
     icon_map = mig.load_icons(atom_icon_dir)
-    
+
     st.write('''
     # Molecule-icons generator!
     ''')
-    
+
     st.write('''
     Generate icons of molecules from Smiles, Names, Cas-number or standard Inchi.
     For more options and information, check out the [GitHub repository](https://github.com/lmonari5/molecule-icon-generator.git) with the python module
     ''')
-    
-    input_type = st.selectbox("Create your icon by", 
-                 ['smiles', 'name', 'cas_number', 'stdinchi'], 
-                 help= 'Chose the input info of your moleculs')
-    
-    input_string = st.text_input('Input informations', "CC(=O)Nc1ccc(cc1)O" )
+
+    input_type = st.selectbox("Create your icon by",
+                              ['smiles', 'name', 'cas_number', 'stdinchi'],
+                              help='Chose the input info of your moleculs')
+
+    input_string = st.text_input('Input informations', "CC(=O)Nc1ccc(cc1)O")
 
     single_bonds = st.checkbox('Draw just single_bonds')
-    remove_H = st.checkbox('remove all Hydrogens') 
+    remove_H = st.checkbox('remove all Hydrogens')
     rdkit_draw = st.checkbox('show rdkit structure')
 
     try:
@@ -60,10 +59,12 @@ if __name__ == "__main__":
 
     try:
         pos_multi = st.slider('Atom position multiplier', 90, 260, 150,
-                help='Multiply the position of the atoms with respect to the 2D structure. Default: 150')
+                              help='''Multiply the position of the atoms with respect to the 2D structure.
+                              The Size of the atom icons will be kept the same, thus a higher multiplier leads to smaller 
+                              images and higher resolution. Default: 150''')
         image = mig.icon_print(smiles, name='molecular-icon', rdkit_img=rdkit_draw,
-                                single_bonds=single_bonds, remove_H=remove_H, save=True,
-                                symbol_img_dict=icon_map, position_multiplier=pos_multi)
+                               single_bonds=single_bonds, remove_H=remove_H, save=True,
+                               symbol_img_dict=icon_map, position_multiplier=pos_multi)
     except Exception as e:
         st.write('''
         Probably Rdkit failed in building the structure of the molecule
@@ -71,27 +72,27 @@ if __name__ == "__main__":
         if st.button('See full error'):
             st.write(e)
         st.stop()
-    
+
     im_rgba = cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA)
     img_list = [im_rgba]
     caption_list = ['Iupac name: ' + iupac]
-    column_widt = 600 
-    
+    column_widt = 600
+
     if rdkit_draw:
         rdkit_img = cv2.imread(os.getcwd() + os.sep + "molecular-icon_rdkit.png", cv2.IMREAD_UNCHANGED)
         rdkit_img = cv2.cvtColor(rdkit_img, cv2.COLOR_BGRA2RGBA)
         img_list.append(rdkit_img)
         caption_list.append('Rdkit 2D conformation')
         column_widt = 300
-        
-    st.image(img_list, caption = caption_list,  width=column_widt,  channels = 'RGBA')
-    
+
+    st.image(img_list, caption=caption_list, width=column_widt, channels='RGBA')
+
     with open(os.getcwd() + os.sep + filename, "rb") as file:
-        btn = st.download_button( label="Download icon",
+        btn = st.download_button(label="Download icon",
                                  data=file,
                                  file_name=filename,
-                                 mime="image/png" )
-    
+                                 mime="image/png")
+
     st.write('''
     Thanks for using the Molecules icons generators!
     ''')
