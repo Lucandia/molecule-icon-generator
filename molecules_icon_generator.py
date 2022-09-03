b1a7898d263c27af3e87714a545add0f289c3c25
lmonari5 <101123231+lmonari5@users.noreply.github.com>
2022-09-04 00:08:12 +0200

    Update molecules_icon_generator.py

--- molecules_icon_generator.py
+++ molecules_icon_generator.py
@@ -59,7 +59,7 @@ def add_image(src, new, position, overwrite=True):
 def add_bond(src, bond_type, degree, position, length):
     # resize the bond length to make it reach the atoms center
     heigth = bond_type.shape[0]
-    if legth > heigth * 1.5:
+    if length > heigth * 1.5:
         # add some thicknes otherwise the rotation of the image could fail
         heigth = int( height * (1 + length/heigth/10) )
     resized_bond = cv2.resize(bond_type.copy(), (length, heigth), interpolation=cv2.INTER_AREA)
