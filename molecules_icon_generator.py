c13948383a4181031af7f3a9e9ef2da97ef1c0ac
lmonari5 <101123231+lmonari5@users.noreply.github.com>
2022-09-04 13:57:03 +0200

    Update molecules_icon_generator.py

--- molecules_icon_generator.py
+++ molecules_icon_generator.py
@@ -129,9 +129,9 @@ def icon_print(SMILES, name='molecule_icon', directory=os.getcwd(), rdkit_img=Fa
             length = int(math.dist([x1, y1], [x2, y2]))
             print(length)
             b_type = BOND.GetBondType()
-            bond_img = symbol_img_dict['single']
+            bond_img = symbol_img_dict['single_bond']
             if rdkit.Chem.rdchem.BondType.DOUBLE == b_type and not single_bonds:
-                bond_img = symbol_img_dict['double']
+                bond_img = symbol_img_dict['double_bond']
             elif rdkit.Chem.rdchem.BondType.AROMATIC == b_type and not single_bonds:
                 conditions = [atom1 not in aromatic_index, atom2 not in aromatic_index,
                               atom_type_map[atom1] != 'O', atom_type_map[atom2] != 'O',
@@ -139,11 +139,11 @@ def icon_print(SMILES, name='molecule_icon', directory=os.getcwd(), rdkit_img=Fa
                               atom_type_map[atom1] != 'N' or atom_bond_map[atom1] < 3,
                               atom_type_map[atom2] != 'N' or atom_bond_map[atom2] < 3]
                 if all(conditions):
-                    bond_img = symbol_img_dict['double']
+                    bond_img = symbol_img_dict['double_bond']
                     aromatic_index.add(atom1)
                     aromatic_index.add(atom2)
             elif rdkit.Chem.rdchem.BondType.TRIPLE == b_type and not single_bonds:
-                bond_img = symbol_img_dict['triple']
+                bond_img = symbol_img_dict['triple_bond']
             add_bond(img, bond_img, mydegrees, position, length)
 
     # add atoms (to start from the Hydrogens, the atom index must be reversed)
