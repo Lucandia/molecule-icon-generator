8c72d40b8a7891ae6be30c098de51577021169cb
lmonari5 <101123231+lmonari5@users.noreply.github.com>
2022-09-04 03:07:11 +0200

    Update molecules_icon_generator.py

--- molecules_icon_generator.py
+++ molecules_icon_generator.py
@@ -33,10 +33,10 @@ def load_icons(folder, resize_dim=(300, 300)):
 icon_map = load_icons(atom_icon_dir)
 
 
-def rotate_image(image, angle):
+def rotate_image(image, angle, output_shape):
     image_center = tuple(np.array(image.shape[1::-1]) / 2) # the '//' division gives error in getRotationMatrix2D
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1])
+    result = cv2.warpAffine(image, rot_mat, output_shape)
     return result
 
 
@@ -66,7 +66,7 @@ def add_bond(src, bond_type, degree, position, dimension_bond):
     #  array_list = [one_column] * length
     # array_list.append(bond_type)
     # resized_bond = np.hstack(array_list)
-    rotated_bond = rotate_image(resized_bond, degree)
+    rotated_bond = rotate_image(resized_bond, degree, src.shape)
     add_image(src, rotated_bond, position)
 
 
