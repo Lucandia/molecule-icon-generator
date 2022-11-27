39733e782dd83ad4eab958dc237e4a27798e04d3
lmonari5 <monarluca@gmail.com>
2022-11-27 12:41:38 +0100

    added reset coloring

--- streamlit-app.py
+++ streamlit-app.py
@@ -58,17 +58,15 @@ if __name__ == "__main__":
         if img_form == img_format:
             forms[ind] = True
 
-    col1, col2, col3 = st.columns(3, gap='medium')
+    col1, col2 = st.columns(2, gap='medium')
     with col1:
         atom_color = st.selectbox(
             'Change the color:',
             list(mig.color_map.keys()))
     with col2:
         new_color[atom_color] = st.color_picker(atom_color, mig.color_map[atom_color], label_visibility="hidden")
-    with col3:
-        st.text('Reset colours as default CPK')
-        if st.button('Reset'):
-            new_color = mig.color_map.copy()
+    if st.button('Reset colours', help='Reset colours as default CPK'):
+        new_color = mig.color_map.copy()
 
     # catch error when using the cirpy library
     try:
