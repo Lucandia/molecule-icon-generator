39f0cca42bc738a4263b2773fda9ddaeb3bec847
lmonari5 <monarluca@gmail.com>
2022-11-28 20:52:50 +0100

    ignore DOS warning, added thickness

--- molecules_icon_generator.py
+++ molecules_icon_generator.py
@@ -133,6 +133,7 @@ def icon_print(SMILES, name='molecule_icon', directory=os.getcwd(), rdkit_png=Fa
     elif bw:
         atom_color = {key: '#ffffff' for key in atom_color}
         atom_color['bond'] = '#000000'
+    print(atom_color.keys())
 
     if '.svg' in name:
         fullname = directory + os.sep + name
