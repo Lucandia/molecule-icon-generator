629be7e4d6942b4f739a1d810eaa541866b7c397
lmonari5 <101123231+lmonari5@users.noreply.github.com>
2022-12-02 17:59:30 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -55,7 +55,8 @@ if __name__ == "__main__":
     Generate icons of molecules from SMILES, Name, Cas-number, Inchi or InChIKey.
     ''')
     st.markdown(
-       "For more options and information, check out the [GitHub repository](https://github.com/lmonari5/molecule-icon-generator.git) \ [DOI](https://doi.org/10.5281/zenodo.7388429): 10.5281/ZENODO.7388429.")
+       "For more options and information, check out the [GitHub repository](https://github.com/lmonari5/molecule-icon-generator.git) \ 
+       [DOI](https://doi.org/10.5281/zenodo.7388429): 10.5281/ZENODO.7388429.")
 
     input_type = st.selectbox("Create your icon by",
                               ['name', 'smiles', 'cas_number', 'stdinchi', 'stdinchikey'],
